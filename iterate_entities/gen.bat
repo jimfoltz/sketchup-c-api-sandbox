87091d1aeb2b2dd7ebeb6ba506be3a69b4452c77
Jim Foltz <jim.foltz@gmail.com>
2017-02-28 03:54:56 -0500

    update gen tool

--- iterate_entities/gen.bat
+++ iterate_entities/gen.bat
@@ -1,25 +1,25 @@
 @echo off
 
-echo 1. Win64
-echo 2. Win32
-echo 3. MinGW
+echo 1. Visual Studio win64
+echo 2. Visual Studio win32
+echo 3. MinGW Makefiles
 
 set /p generator=
 
 if %generator%==1 (
-	rd /q /s build-Win64
-	md build-Win64
-	cd build-Win64
+	rd /q /s build-win64
+	md build-win64
+	cd build-win64
 	cmake -G "Visual Studio 14 2015 Win64" ..
 ) else if %generator%==2 (
-	rd /q /s build-Win32
-	md build-Win32
-	cd build-Win32
+	rd /q /s build-win32
+	md build-win32
+	cd build-win32
 	cmake -G "Visual Studio 14 2015" ..
 ) else if %generator%==3 (
-	rd /q /s build-MinGW
-	md build-MinGW
-	cd build-MinGW
+	rd /q /s build-mingw
+	md build-Mingw
+	cd build-Mingw
 	cmake -G "MinGW Makefiles" ..
 )
 
